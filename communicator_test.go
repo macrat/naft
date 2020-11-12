b9351a5c1f1018ef607e848f6e20a579579e1ca9
MacRat <m@crat.jp>
2020-11-12 21:27:10 +0900

    Added tests for communicate helper functions

---
+++ communicator_test.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func MakeHosts(hosts ...string) []*Host {
+	hs := make([]*Host, len(hosts))
+	for i, h := range hosts {
+		hs[i] = MustParseHost(h)
+	}
+	return hs
+}
+
+type DummyCommunicator []*Host
+
+func (dc DummyCommunicator) test(target *Host) error {
+	for _, h := range dc {
+		if h.Equals(target) {
+			return nil
+		}
+	}
+	return fmt.Errorf("%s is not listed in allows", target)
+}
+
+func (dc DummyCommunicator) SendLogAppend(target *Host, l LogAppendMessage) error {
+	return dc.test(target)
+}
+
+func (dc DummyCommunicator) SendRequestVote(target *Host, l VoteRequestMessage) error {
+	return dc.test(target)
+}
+
+func TestOperateToAllHosts(t *testing.T) {
+	hs := MakeHosts("http://localhost:5000", "http://localhost:5001", "http://localhost:5002", "http://localhost:5003")
+
+	err := OperateToAllHosts(DummyCommunicator{}, hs, 4, func(m MessageSender, h *Host, agree chan bool) {
+		agree <- true
+	})
+	if err != nil {
+		t.Errorf("expected success but got error: %s", err)
+	}
+
+	err = OperateToAllHosts(DummyCommunicator(hs[:1]), hs, 4, func(m MessageSender, h *Host, agree chan bool) {
+		agree <- m.SendLogAppend(h, LogAppendMessage{}) == nil
+	})
+	if err == nil {
+		t.Errorf("expected failure but succeed")
+	} else if err.Error() != "need least 4 hosts agree but only 1 hosts agreed" {
+		t.Errorf("unexpected error: %s", err)
+	}
+
+	err = OperateToAllHosts(DummyCommunicator(hs[:2]), hs, 3, func(m MessageSender, h *Host, agree chan bool) {
+		agree <- m.SendLogAppend(h, LogAppendMessage{}) == nil
+	})
+	if err == nil {
+		t.Errorf("expected failure but succeed")
+	} else if err.Error() != "need least 3 hosts agree but only 2 hosts agreed" {
+		t.Errorf("unexpected error: %s", err)
+	}
+
+	err = OperateToAllHosts(DummyCommunicator(hs[:2]), hs, 2, func(m MessageSender, h *Host, agree chan bool) {
+		agree <- m.SendLogAppend(h, LogAppendMessage{}) == nil
+	})
+	if err != nil {
+		t.Errorf("expected success but got error: %s", err)
+	}
+
+	err = OperateToAllHosts(DummyCommunicator(hs[:2]), []*Host{}, 2, func(m MessageSender, h *Host, agree chan bool) {
+		agree <- m.SendLogAppend(h, LogAppendMessage{}) == nil
+	})
+	if err != nil {
+		t.Errorf("expected success but got error: %s", err)
+	}
+}
+
+func TestSendAppendToAllHosts(t *testing.T) {
+	hs := MakeHosts("http://localhost:5000", "http://localhost:5001", "http://localhost:5002", "http://localhost:5003")
+	dc := DummyCommunicator(hs[:2])
+
+	err := SendLogAppendToAllHosts(dc, hs, 2, LogAppendMessage{})
+	if err != nil {
+		t.Errorf("expected success but got error: %s", err)
+	}
+
+	err = SendLogAppendToAllHosts(dc, hs, 3, LogAppendMessage{})
+	if err == nil {
+		t.Errorf("expected failure but succeed")
+	} else if err.Error() != "need least 3 hosts agree but only 2 hosts agreed" {
+		t.Errorf("unexpected error: %s", err)
+	}
+}
+
+func TestRequestVoteToAllHosts(t *testing.T) {
+	hs := MakeHosts("http://localhost:5000", "http://localhost:5001", "http://localhost:5002", "http://localhost:5003")
+	dc := DummyCommunicator(hs[:2])
+
+	err := SendRequestVoteToAllHosts(dc, hs, 2, VoteRequestMessage{})
+	if err != nil {
+		t.Errorf("expected success but got error: %s", err)
+	}
+
+	err = SendRequestVoteToAllHosts(dc, hs, 3, VoteRequestMessage{})
+	if err == nil {
+		t.Errorf("expected failure but succeed")
+	} else if err.Error() != "need least 3 hosts agree but only 2 hosts agreed" {
+		t.Errorf("unexpected error: %s", err)
+	}
+}
