978c31b7cfc3963280db44a390c83e66cfd29f9b
MacRat <m@crat.jp>
2020-11-15 14:48:55 +0900

    Removed unused method

--- term.go
+++ term.go
@@ -12,10 +12,3 @@ type Term struct {
 func (t Term) String() string {
 	return fmt.Sprintf("[%d](%s)", t.ID, t.Leader)
 }
-
-func (t Term) Equals(another Term) bool {
-	if t.Leader == nil || another.Leader == nil {
-		return false
-	}
-	return t.ID == another.ID && t.Leader.Equals(another.Leader)
-}
